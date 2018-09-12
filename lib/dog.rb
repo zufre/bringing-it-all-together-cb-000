8c4e519997fe2a4e9b082def9fef7397dbb6f55b
Cedric David <cedric.david@btopenworld.com>
2018-09-12 03:34:06 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -36,4 +36,11 @@ class Dog
       dog.save
       dog
   end
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE id = ?
+    SQL
+    DB[:conn].execute(sql, id)
+  end
 end
