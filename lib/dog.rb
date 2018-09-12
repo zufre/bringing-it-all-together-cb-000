ea0583a6502e523fd8c69656e77c20f133e6b5c3
Cedric David <cedric.david@btopenworld.com>
2018-09-12 04:11:16 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -51,4 +51,8 @@ class Dog
       self.new_from_db(row)
     end.first
   end
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed)
+  end
 end
